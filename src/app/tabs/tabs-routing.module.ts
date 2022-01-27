bec6cfe83c10f40ae1a2dd52728da1dfefa3000c
Rares Golea <rgolea@users.noreply.github.com>
2022-01-27 20:56:12 +0100

    Small changes

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -32,6 +32,10 @@ const routes: Routes = [
       {
         path: 'statistics',
         loadChildren: () => import('../statistics/statistics.module').then(m => m.StatisticsModule)
+      },
+      {
+        path: '**',
+        redirectTo: 'home'
       }
     ]
   },
